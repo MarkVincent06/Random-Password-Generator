68f8c52527193aa56d6e3edee7ae6e5e926f17d8
Blueboltz <vincentmariscotescleofe@gmail.com>
2022-10-21 00:09:21 +0800

    Added styling and js to generate random passwords

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+const passwordBtn = document.getElementById("hero--pass-btn");
+const passField1 = document.getElementById("pass-field-1");
+const passField2 = document.getElementById("pass-field-2");
+
+let randomPassword;
+
+passwordBtn.addEventListener("click", () => {
+    passField1.textContent = getRandomPassword();
+    passField2.textContent = getRandomPassword();
+});
+
+function getRandomPassword() {
+    let randomPassword = "";
+    for(let i=0; i<15; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        randomPassword += characters[randomIndex]; 
+    } 
+    return randomPassword;
+}
